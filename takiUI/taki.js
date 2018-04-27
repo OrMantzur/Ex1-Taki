ec8af80c6b9dc2f7eaf8fc231bbcf3537f24b78b
OrMantzur <or.mantzur@gmail.com>
2018-04-27 17:07:48 +0300

    separate to container

--- takiUI/taki.js
+++ takiUI/taki.js
@@ -3,7 +3,7 @@
  * Or Mantzur - 204311997
  */
 
-function drawCardOnScreenNew() {
+function drawCardOnScreen() {
     var playerCards = game.getPlayer(0).getCards();
     var playerCardsContainer = document.getElementById("playerCardsContainer");
     var cardIndex = 0;
@@ -47,39 +47,10 @@ function drawCardOnScreenNew() {
     //     // cardElement.style = "color: " + card.getColor();
     //     playerCardsContainer.appendChild(cardElement);
     // }
-};
-
-function drawCardOnScreen(parentId, playerId) {
-    var playerCards = game.getPlayer(playerId).getCards();
-    // one player cards row at the caller table
-    var cardsRowElement = document.createElement("tr");
-    var cardIndex = 0;
-
-    //clear all children
-    while (cardsRowElement.firstChild) {
-        cardsRowElement.removeChild(cardsRowElement.firstChild);
-    }
-    document.getElementById(parentId).innerHTML = '';
-    playerCards.forEach(function (card) {
-        var cardElement = document.createElement("td");
-        cardElement.setAttribute("id", cardIndex.toString());
-        cardElement.setAttribute("class", "card " + card.getColor());
-        cardIndex++;
-        cardElement.addEventListener("click", function () {
-            game.makeMove(card);
-            refreshCards();
-        });
-        cardElement.textContent = card.getValue();
-        // cardElement.style = "color: " + card.getColor();
-        cardsRowElement.appendChild(cardElement);
-    });
-    document.getElementById(parentId).appendChild(cardsRowElement);
 }
 
 function refreshCards() {
-    drawCardOnScreenNew();
-    // drawCardOnScreen("player1CardsTable", 0);
-    drawCardOnScreen("player2CardsTable", 1);
+    drawCardOnScreen();
     drawTopCard("topCard");
 }
 
